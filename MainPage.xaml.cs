cdb9cd51db134d862c13e2b6d73091f843a99a9e
Miky <vyasmiky@gmail.com>
2022-11-06 19:49:31 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+namespace Disclipper;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		string idval = entry.Text.Split(":")[2].TrimEnd('>');
+		string suffix = ".png";
+		if (entry.Text.StartsWith("<a")){
+			suffix = ".gif";
+		}
+
+		output.Text = $"https://cdn.discordapp.com/emojis/{idval}{suffix}";
+        await Clipboard.Default.SetTextAsync($"https://cdn.discordapp.com/emojis/{idval}{suffix}");
+
+    }
+}
+
